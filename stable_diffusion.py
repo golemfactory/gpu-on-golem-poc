18e21dcd1ec309d4bb07cff9bf14d4ae6ba69858
Marcin Stachura <stan7@tlen.pl>
2022-11-22 16:21:02 +0100

    Stable diffusion requestor script

---
+++ stable_diffusion.py
@@ -0,0 +1,37 @@
+from datetime import timedelta
+import asyncio
+from typing import AsyncIterable
+
+from yapapi import Golem, Task, WorkContext
+from yapapi.log import enable_default_logger
+from yapapi.payload import vm
+
+
+enable_default_logger(log_file="sd-golem.log")
+
+
+async def _worker(context: WorkContext, tasks: AsyncIterable[Task]):
+    script = context.new_script()
+    async for task in tasks:
+        script.run("run.sh", "cuda", task.data['phrase'])
+        future_result = script.download_file('/usr/src/app/output/img.png', f'images/{task.data["id"]}.png')
+        yield script
+        task.accept_result(result=await future_result)
+
+
+async def _generate_on_golem(phrase, job_id):
+    package = await vm.repo(
+        image_hash="5080b041087e342f258955abfa2042cf1a162697b589b00964b923be",
+        image_url='http://116.203.41.115:8000/docker-diffusers-golem-latest-16ef828013.gvmi',
+    )
+
+    tasks = [Task(data={'phrase': phrase, 'id': job_id})]
+
+    async with Golem(budget=10.0, subnet_tag="sd-test") as golem:
+        async for completed in golem.execute_tasks(_worker, tasks, payload=package, timeout=timedelta(minutes=90),
+                                                   max_workers=1):
+            print(completed.result.stdout)
+
+
+def generate_image(phrase, job_id):
+    asyncio.run(_generate_on_golem(phrase, job_id))
