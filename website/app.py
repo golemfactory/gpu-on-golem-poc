0f0539276cfbda171231742f18ac8182f236857a
Marcin Stachura <marcin.stachura@imapp.pl>
2022-11-23 14:54:58 +0100

    First working version

--- website/app.py
+++ website/app.py
@@ -1,34 +1,48 @@
-from fastapi import FastAPI, Request, Form, status
 from typing import Union
-from stable_diffusion import generate_image
+import uuid
 
-import uuid, rq
-
+from fastapi import FastAPI, Request, Form, status
+from fastapi.staticfiles import StaticFiles
+from redis import Redis
+from rq import Queue
+from rq.job import Job
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
-templates = Jinja2Templates(directory="templates")
+from stable_diffusion import generate_image
 
+
+redis_conn = Redis()
+q = Queue(connection=redis_conn)
+templates = Jinja2Templates(directory="templates")
 app = FastAPI()
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
 
 @app.get("/")
-async def home(request: Request):
+async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.post("/text2img/")
-async def add_item_to_queue(request: Request, prompt: str = Form(...)): #TODO How FastAPI knows which string from Form to take? What if there is more "prompt" named text inputs? Are names unique in HTML (same as IDs)?  
+
+@app.post("/txt2img/")
+async def add_job_to_queue(request: Request, prompt: str = Form(...)):
     uuid_str = str(uuid.uuid4())
-    url = app.url_path_for("read_item", uuid=uuid_str)
+    q.enqueue(generate_image, prompt, uuid_str, job_id=uuid_str)
+    url = app.url_path_for("job_detail", job_id=uuid_str)
     url += ("?prompt=" + prompt)
     return RedirectResponse(url=url, status_code=status.HTTP_302_FOUND)
 
-@app.get("/text2img/{uuid}/")
-async def read_item(request: Request, uuid: str, prompt: str = ""):
+
+@app.get("/txt2img/{job_id}/")
+async def job_detail(request: Request, job_id: str, prompt: str = ""):
+    job = Job.fetch(job_id, connection=redis_conn)
+    task_status = job.get_status()
+    img_path = f'/images/{job_id}.png'
     return templates.TemplateResponse(
-        "job-detail.html", 
+        "job-detail.html",
         {
-            "request": request, 
-            "prompt": prompt, 
-            "status": task_status, 
+            "request": request,
+            "prompt": prompt,
+            "status": task_status,
             "img_path": img_path
         })
