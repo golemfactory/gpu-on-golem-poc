546e540e710e8be4b13c70f0e198d458c409df00
Marcin Stachura <marcin.stachura@imapp.pl>
2022-12-09 13:35:58 +0100

    Added server specific asset path prefix

--- client/next.config.js
+++ client/next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { domains: [process.env.NEXT_PUBLIC_HOSTNAME], unoptimized: true },
+  assetPrefix: '/sd/static',
 };
 
 module.exports = nextConfig;
