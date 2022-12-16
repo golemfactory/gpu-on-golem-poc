a5cbabeafe9a50173480b0a50b31f618385a7709
Marcin Stachura <marcin.stachura@imapp.pl>
2022-12-16 17:04:01 +0100

    Clearing status.json file before each generation

--- provider/generate.sh
+++ provider/generate.sh
@@ -2,4 +2,5 @@
 
 cd /usr/src/app
 rm /usr/src/app/output/img.png /usr/src/app/output/iteration_*.jpg /usr/src/app/output/status.json
+echo "{\"progress\": 0}" > /usr/src/app/output/status.json
 echo "$1" > phrase.txt
