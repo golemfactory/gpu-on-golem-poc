13c0dbb299c4e1c0d5b706bca9e2946b23fa1095
Natalia Kulas <nataliakulas@gmail.com>
2022-12-28 21:42:22 +0100

    Text in a loop rendering

--- client/src/components/Result/Result.tsx
+++ client/src/components/Result/Result.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { Status } from 'enums/status';
 import { Copy, Process, View } from 'components';
 import { useStatusState } from 'utils/hooks';
@@ -28,6 +28,26 @@ function Result({
 
   const handleOpen = () => window.open('https://chat.golem.network/');
 
+  const facts = [
+    'Golem aims to be the first real decentralized marketplace for computing. And it seems that its doing its job right now, as real nodes are computing your task!',
+    'Did you know there are some special filters included? No funny pictures here!',
+    'Third fun fact placeholder is here. How fast can you read it?',
+  ];
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setIndex((state: number) => (state !== facts.length - 1 ? state + 1 : 0)), 5000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [facts.length]);
+
+  const renderText = (children: ReactNode) => (
+    <p className="mt-[5.7rem] mb-[2.4rem] min-h-[9.5rem] text-14">{children}</p>
+  );
+
   return (
     <div className="mt-[12rem] mb-[8rem]">
       <h2 className="text-36">Result for:</h2>
@@ -43,27 +63,28 @@ function Result({
           <br />
           <br />
           <Process data={data} nodes={nodes} />
-          <p className="mt-[5.7rem] mb-[2.4rem] text-14">
-            Fun facts:
-            <br />
-            <br />
-            Golem aims to be the first real decentralized marketplace for computing. And it seems that its doing its job
-            right now, as real nodes are computing your task!
-          </p>
+          {renderText(
+            <>
+              Fun facts:
+              <br />
+              <br />
+              {facts[index]}
+            </>,
+          )}
         </div>
       )}
       {forState([Status.Finished]) && (
         <div className="mx-auto w-[85%] md:w-[75%]">
           <View src={src} value={value} />
-          <p className="mt-[5.7rem] mb-[2.4rem] text-14">
-            We hope you enjoy your results! Drop us a line on our Discord chat and share your experience :)
-          </p>
+          {renderText(
+            'We hope you enjoy your results! Drop us a line on our Discord chat and share your experience :)',
+          )}
         </div>
       )}
       {forState([Status.Blocked]) && (
         <div className="mx-auto w-[85%] md:w-[75%]">
           <View src={src} value={value} blocked />
-          <p className="mt-[5.7rem] mb-[2.4rem] text-14">Sorry, this image violates our NSFW filters :(</p>
+          {renderText('Sorry, this image violates our NSFW filters :(')}
         </div>
       )}
       <div className="mb-[1.8rem]">
