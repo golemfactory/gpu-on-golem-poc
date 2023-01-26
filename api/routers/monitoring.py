3eb9da27cf32a472ee759320ead69e88a17514bd
Marcin Stachura <stan7@tlen.pl>
2023-01-26 12:59:29 +0100

    Added flag to hold user off for a longer time when queue is full

--- api/routers/monitoring.py
+++ api/routers/monitoring.py
@@ -6,7 +6,7 @@ from websockets import ConnectionClosed
 
 from api.redis_functions import get_service_data, jobs_queue
 
-QUEUE_STATE_WS_REFRESH_SECONDS = 10
+QUEUE_STATE_WS_REFRESH_SECONDS = 5
 
 router = APIRouter()
 
@@ -28,10 +28,23 @@ async def get_queue_length(request: Request):
 async def get_queue_length_ws(websocket: WebSocket):
     await websocket.accept()
 
+    hold_off_user = False
     try:
         while True:
             queue_size = await jobs_queue.qsize()
-            await websocket.send_json({"jobs_in_queue": queue_size, 'max_queue_size': jobs_queue.max_size})
+
+            if hold_off_user and queue_size < 0.7 * jobs_queue.max_size:
+                hold_off_user = False
+
+            if queue_size >= jobs_queue.max_size:
+                hold_off_user = True
+
+            msg = {
+                "jobs_in_queue": queue_size,
+                'max_queue_size': jobs_queue.max_size,
+                'hold_off_user': hold_off_user,
+            }
+            await websocket.send_json(msg)
             await asyncio.sleep(QUEUE_STATE_WS_REFRESH_SECONDS)
     except ConnectionClosed:
         pass
