8f4d0253711c2c7d842327312265c82a81d59f34
Marcin Stachura <marcin.stachura@imapp.pl>
2023-03-09 13:57:43 +0100

    Automatic proxy for renting before tests

--- rent_gpu/requestor/automatic_proxy.py
+++ rent_gpu/requestor/automatic_proxy.py
@@ -1,23 +1,24 @@
 import asyncio
-from datetime import timedelta
+import datetime
 import pathlib
-import random
-import string
 import sys
 
 from sqlmodel import Session, select
 from yapapi import Golem
-from yapapi.contrib.service.socket_proxy import SocketProxy, SocketProxyService
+from yapapi.contrib.service.http_proxy import HttpProxyService, LocalHttpProxy
 from yapapi.payload import vm
+from yapapi.services import ServiceState
 from yapapi.strategy import SCORE_REJECTED, SCORE_TRUSTED, MarketStrategy
 
 from rent_gpu.requestor.db import engine, Offer, OfferStatus
 
 
+CLUSTER_EXPIRATION_TIME = datetime.timedelta(days=365)
+
 examples_dir = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(str(examples_dir))
 
-# TODO: przebudować łącząc ssh i automatic
+
 class ConcreteProviderStrategy(MarketStrategy):
     def __init__(self, provider_id):
         self.provider_id = provider_id
@@ -26,19 +27,13 @@ class ConcreteProviderStrategy(MarketStrategy):
         return SCORE_TRUSTED if offer.issuer == self.provider_id else SCORE_REJECTED
 
 
-class SshService(SocketProxyService):
-    remote_port = 22
-
-    def __init__(self, proxy: SocketProxy):
-        super().__init__()
-        self.proxy = proxy
-
+class AutomaticService(HttpProxyService):
     @staticmethod
     async def get_payload():
         return await vm.repo(
-            image_hash='856a5ed05eb3055e2b130a44e104d2f34f0a28fa8fa5ae7a958f9cea',
-            image_url='http://gpu-on-golem.s3.eu-central-1.amazonaws.com/rent_gpu-856a5ed05eb3055e2b130a44e104d2f34f0a28fa8fa5ae7a958f9cea.gvmi',
-            capabilities=[vm.VM_CAPS_VPN],
+            image_hash='7ed8fdaf564554e88b9312ed307a80b60a7aa9671ed1735f8155d9f1',
+            image_url='http://gpu-on-golem.s3.eu-central-1.amazonaws.com/docker-automatic-golem-7ed8fdaf564554e88b9312ed307a80b60a7aa9671ed1735f8155d9f1.gvmi',
+            capabilities=[vm.VM_CAPS_VPN, 'cuda*'],
         )
 
     async def start(self):
@@ -47,29 +42,19 @@ class SshService(SocketProxyService):
         async for script in super().start():
             yield script
 
-        password = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
-
-        script = self._ctx.new_script(timeout=timedelta(seconds=10))
-        script.run("/bin/bash", "-c", "ssh-keygen -A")
-        script.run("/bin/bash", "-c", f'echo -e "{password}\n{password}" | passwd')
-        script.run("/bin/bash", "-c", "/usr/sbin/sshd")
+        script = self._ctx.new_script()
+        script.run("/usr/src/app/run_service.sh", "127.0.0.1", "8000")
         yield script
 
-        server = await self.proxy.run_server(self, self.remote_port)
+        script = self._ctx.new_script()
+        script.run("/usr/sbin/nginx")
+        yield script
 
         with Session(engine) as session:
             offer = session.exec(select(Offer).where(Offer.provider_id == self.provider_id)).one()
             offer.status = OfferStatus.READY
-            offer.password = password
             session.add(offer)
             session.commit()
-
-        print(
-            f"connect with:\n"
-            f"ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "
-            f"-p {server.local_port} root@{server.local_address}"
-        )
-        print(f"password: {password}")
 
 
 async def main(provider_id: str, local_port: int):
@@ -79,18 +64,26 @@ async def main(provider_id: str, local_port: int):
     """
 
     async with Golem(budget=1.0, subnet_tag='gpu-test', strategy=ConcreteProviderStrategy(provider_id)) as golem:
-
         network = await golem.create_network("192.168.0.1/24")
-        proxy = SocketProxy(address='0.0.0.0', ports=[local_port])
 
         async with network:
             cluster = await golem.run_service(
-                SshService,
+                AutomaticService,
                 network=network,
+                expiration=datetime.datetime.now() + CLUSTER_EXPIRATION_TIME,
                 num_instances=1,
-                instance_params=[{"proxy": proxy}],
             )
             instances = cluster.instances
+
+            def still_starting():
+                return any(i.state in (ServiceState.pending, ServiceState.starting) for i in instances)
+
+            while still_starting():
+                print('Starting')
+                await asyncio.sleep(5)
+
+            proxy = LocalHttpProxy(cluster, local_port)
+            await proxy.run()
 
             while True:
                 print(instances)
