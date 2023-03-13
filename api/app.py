4726d668508aaa2e8292a075a63848d456f8d01c
Adam Mizerski <adam@mizerski.pl>
2023-03-13 19:58:29 +0100

    allow to keep git repo clean (#34)

--- api/app.py
+++ api/app.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from fastapi import FastAPI, Request, status
@@ -20,12 +21,16 @@ async def throttling_exception_handler(request: Request, exc: RateLimitExceeded)
     return JSONResponse({'error': 'Too many requests.'}, status_code=status.HTTP_429_TOO_MANY_REQUESTS)
 
 
+data_dir = Path(os.environ.get("GPUOG_DATA_DIR") or Path(__file__).parent)
+frontend_dir = Path(os.environ.get("GPUOG_FRONTEND_DIR") or Path(__file__).parent)
+
+
 limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
 app.include_router(jobs.router)
 app.include_router(monitoring.router)
-app.mount("/images", StaticFiles(directory=Path(__file__).parent.joinpath('images').resolve()), name="images")
-app.mount("/static", StaticFiles(directory=Path(__file__).parent.joinpath('static').resolve()), name="static")
+app.mount("/images", StaticFiles(directory=data_dir.joinpath('images').resolve()), name="images")
+app.mount("/static", StaticFiles(directory=frontend_dir.joinpath('static').resolve()), name="static")
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, throttling_exception_handler)
 app.add_middleware(
@@ -38,6 +43,6 @@ app.add_middleware(
 
 @app.get("/")
 async def index(request: Request):
-    with open(Path(__file__).parent.joinpath('static/index.html'), 'r') as f:
+    with open(frontend_dir.joinpath('static/index.html'), 'r') as f:
         index_html = f.read()
     return HTMLResponse(content=index_html, status_code=status.HTTP_200_OK)
