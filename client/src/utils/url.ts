86ab4dd9d365bd8feffc0959ec5a8b956d3aac64
Marcin Stachura <stan7@tlen.pl>
2023-03-27 12:18:44 +0200

    images URL fix

--- client/src/utils/url.ts
+++ client/src/utils/url.ts
@@ -1,4 +1,4 @@
 const url = (path: string, ws: boolean, id?: string) =>
-  `${ws ? 'wss' : 'https'}://${process.env.NEXT_PUBLIC_HOSTNAME}/${path}${ws ? '/ws' : ''}/${id ? id + '/' : ''}`;
+  `${ws ? 'wss' : 'https'}://${process.env.NEXT_PUBLIC_HOSTNAME}/${path}${ws ? '/ws' : ''}/${id ? id + (ws ? '/' : '') : ''}`;
 
 export default url;
