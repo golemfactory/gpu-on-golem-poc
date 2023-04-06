d4c69d2a2b3e857ae543900241610439d41b48c0
Marcin Stachura <marcin.stachura@imapp.pl>
2023-04-06 11:25:29 +0200

    Fix for URL creation in client

--- client/src/utils/url.ts
+++ client/src/utils/url.ts
@@ -1,4 +1,7 @@
-const url = (path: string, ws: boolean, id?: string) =>
-  `${ws ? 'wss' : 'https'}://${process.env.NEXT_PUBLIC_HOSTNAME}/${path}${ws ? '/ws' : ''}/${id ? id + (ws ? '/' : '') : ''}`;
+const url = (path: string, ws: boolean, id?: string) =>{
+    let url = `${ws ? 'wss' : 'https'}://${process.env.NEXT_PUBLIC_HOSTNAME}/${path}${ws ? '/ws' : ''}/${id ? id + '/' : ''}`;
+    return !ws && url.endsWith('/') ? url.slice(0, -1) : url;
+}
+
 
 export default url;
