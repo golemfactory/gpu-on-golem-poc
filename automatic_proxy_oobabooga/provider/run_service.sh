0274611ee1d5f5f552cf723826c08616d2bbfb8e
Marcin Stachura <marcin.stachura@imapp.pl>
2023-06-14 16:02:57 +0200

    oobabooga requestor - WIP

--- automatic_proxy_oobabooga/provider/run_service.sh
+++ automatic_proxy_oobabooga/provider/run_service.sh
@@ -2,4 +2,4 @@
 # Znaleźć miejsce w którym odpala się oobabooga-webui
 
 cd /home/python_user/text-generation-webui
-python3 server.py --cpu --verbose --api --model facebook_opt-1.3b> out.txt 2>&1 &
+python3 server.py --cpu --verbose --api --api-blocking-port "$1" --listen --listen-host "$2" --listen-port "$3" --model facebook_opt-1.3b > out.txt 2>&1 &
