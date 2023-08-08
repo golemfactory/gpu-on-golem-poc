8ff69c041c1da8c34988fab410f4428650ca4a8a
Marcin Stachura <marcin.stachura@imapp.pl>
2023-08-08 14:37:05 +0200

    Proper command called

--- rent_golem/backend/build_docker.sh
+++ rent_golem/backend/build_docker.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) backend
-docker-compose build
+docker compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) backend
+docker compose build
