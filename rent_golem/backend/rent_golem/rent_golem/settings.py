b50f0454598e3b55e42f04b4ae285abe52d0c78c
Marcin Stachura <stan7@tlen.pl>
2023-08-21 22:30:33 +0200

    Cache added

--- rent_golem/backend/rent_golem/rent_golem/settings.py
+++ rent_golem/backend/rent_golem/rent_golem/settings.py
@@ -93,6 +93,16 @@ DATABASES = {
 }
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get("CACHE_BACKEND_URL", "redis://redis:6379/0"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
