8f641073b2c52b331b31e199d77efdb5cde6339a
Marcin Stachura <marcin.stachura@imapp.pl>
2023-11-09 11:04:53 +0100

    Limit scanning offers to vm-nvidia runtime only

--- rent_gpu/requestor/scanner.py
+++ rent_gpu/requestor/scanner.py
@@ -3,7 +3,6 @@ from asyncio import TimeoutError
 from datetime import datetime, timezone
 import pathlib
 import sys
-from typing import Optional
 
 from sqlmodel import Session, select, delete
 from yapapi import props as yp
@@ -18,19 +17,37 @@ examples_dir = pathlib.Path(__file__).resolve().parent.parent
 sys.path.append(str(examples_dir))
 
 
-def get_gpu_card(offer: OfferProposal) -> Optional[str]:
+def has_vm_nvidia_runtime(offer: OfferProposal) -> bool:
     capabilities = offer.props.get("golem.runtime.capabilities", [])
-    # Try to detect CUDA card from Norbert's implementation
-    cuda_card = next((cap.split(',', 1)[1].strip()
-                      for cap in capabilities
-                      if cap.startswith('cuda,')),
-                     None)
+    return offer.props.get("golem.runtime.name", '') == 'vm-nvidia' and '!exp:gpu' in capabilities
 
-    # Try to detect CUDA card from ITL's implementation
-    if cuda_card is None and '!exp:gpu' in capabilities:
-        cuda_card = offer.props.get("golem.!exp.gap-35.v1.inf.gpu.model", None)
 
-    return cuda_card
+def delete_current_offers():
+    with Session(engine) as session:
+        stmt = delete(Offer).where(Offer.status == OfferStatus.FREE)
+        session.execute(stmt)
+        session.commit()
+
+
+def create_offer_from_proposal(event: OfferProposal):
+    gpu_card = event.props.get("golem.!exp.gap-35.v1.inf.gpu.model", None)
+    with Session(engine) as session:
+        existing_offer = session.exec(select(Offer).where(Offer.provider_id == event.issuer)).first()
+        if existing_offer:
+            existing_offer.name = event.props.get('golem.node.id.name')
+            existing_offer.card = gpu_card
+            existing_offer.memory = float(event.props.get('golem.inf.mem.gib'))
+            session.add(existing_offer)
+        else:
+            session.add(
+                Offer(
+                    provider_id=event.issuer,
+                    name=event.props.get('golem.node.id.name'),
+                    card=gpu_card,
+                    memory=float(event.props.get('golem.inf.mem.gib')),
+                )
+            )
+        session.commit()
 
 
 async def list_offers(conf: Configuration, subnet_tag: str):
@@ -39,36 +56,17 @@ async def list_offers(conf: Configuration, subnet_tag: str):
         dbuild = DemandBuilder()
         dbuild.add(yp.NodeInfo(name="Scanner node", subnet_tag=subnet_tag))
         dbuild.add(yp.Activity(expiration=datetime.now(timezone.utc)))
+
+        delete_current_offers()
+
         offers = set()
-
-        with Session(engine) as session:
-            stmt = delete(Offer).where(Offer.status == OfferStatus.FREE)
-            session.execute(stmt)
-            session.commit()
-
         async with market_api.subscribe(dbuild.properties, dbuild.constraints) as subscription:
             async for event in subscription.events():
-                gpu_card = get_gpu_card(event)
-                if gpu_card and event.issuer not in offers:
+                if has_vm_nvidia_runtime(event) and event.issuer not in offers:
                     offers.add(event.issuer)
+                    gpu_card = event.props.get("golem.!exp.gap-35.v1.inf.gpu.model", None)
+                    create_offer_from_proposal(event)
                     print(f"Provider: {event.issuer} , Name: {event.props.get('golem.node.id.name')}, Card: {gpu_card}")
-                    with Session(engine) as session:
-                        existing_offer = session.exec(select(Offer).where(Offer.provider_id == event.issuer)).first()
-                        if existing_offer:
-                            existing_offer.name = event.props.get('golem.node.id.name')
-                            existing_offer.card = gpu_card
-                            existing_offer.memory = float(event.props.get('golem.inf.mem.gib'))
-                            session.add(existing_offer)
-                        else:
-                            session.add(
-                                Offer(
-                                    provider_id=event.issuer,
-                                    name=event.props.get('golem.node.id.name'),
-                                    card=gpu_card,
-                                    memory=float(event.props.get('golem.inf.mem.gib')),
-                                )
-                            )
-                        session.commit()
 
 
 def main(subnet: str):
