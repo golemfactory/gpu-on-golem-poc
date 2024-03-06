ea164478a9f83adb663125427aad6e1687bece43
Marcin Stachura <stan7@tlen.pl>
2024-03-06 19:43:03 +0100

    Cleanup

--- diffusers/provider/service.py
+++ diffusers/provider/service.py
@@ -4,7 +4,6 @@ import logging
 import sys
 
 from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, EulerDiscreteScheduler
-from PIL import Image
 from safetensors.torch import load_file
 import portalocker
 import torch
@@ -18,7 +17,6 @@ logging.basicConfig(filename='output/debug.log',
 logger = logging.getLogger(__name__)
 
 STABLE_DIFFUSION_ITERATIONS_NUMBER = 5
-intermediary_images_number: int = 0
 
 
 def handle_exception(exc_type, exc_value, exc_traceback):
@@ -33,23 +31,10 @@ sys.excepthook = handle_exception
 
 unet = UNet2DConditionModel.from_config("./stable-diffusion-xl-base-1.0", subfolder="unet").to("cuda", torch.float16)
 unet.load_state_dict(load_file("./sdxl_lightning_4step_unet.safetensors"))
-pipe = StableDiffusionXLPipeline.from_pretrained("./stable-diffusion-xl-base-1.0", unet=unet, torch_dtype=torch.float16, variant="fp16").to(
-    "cuda")
-vae = pipe.vae
+pipe = StableDiffusionXLPipeline.from_pretrained("./stable-diffusion-xl-base-1.0", unet=unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
 
 # Ensure sampler uses "trailing" timesteps.
 pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config, timestep_spacing="trailing")
-
-
-def latents_to_pil(latents):
-    latents = (1 / 0.18215) * latents
-    with torch.no_grad():
-        image = vae.decode(latents).sample
-    image = (image / 2 + 0.5).clamp(0, 1)
-    image = image.detach().cpu().permute(0, 2, 3, 1).numpy()
-    images = (image * 255).round().astype("uint8")
-    pil_images = [Image.fromarray(image) for image in images]
-    return pil_images
 
 
 def latents_callback(i, t, latents):
@@ -58,20 +43,6 @@ def latents_callback(i, t, latents):
     progress = progress if progress < 100 else None
 
     intermediary_img_path = None
-    if intermediary_images_number > 0:
-        iterations_to_generate = set(
-            list(
-                range(0, STABLE_DIFFUSION_ITERATIONS_NUMBER - 1,
-                      int((STABLE_DIFFUSION_ITERATIONS_NUMBER - 1) / intermediary_images_number)
-                      )
-            )[:intermediary_images_number]
-        )
-        if i in iterations_to_generate:
-            image = latents_to_pil(latents)
-            rgb_img = image[0].convert('RGB').resize((256, 256))
-            intermediary_img_path = f"output/iteration_{i}.jpg"
-            rgb_img.save(intermediary_img_path, optimize=True, quality=50)
-
     update_status(progress=progress, new_image_path=intermediary_img_path)
 
 
@@ -123,5 +94,4 @@ async def main():
 
 
 if __name__ == '__main__':
-    intermediary_images_number = int(sys.argv[1])
     asyncio.run(main())
